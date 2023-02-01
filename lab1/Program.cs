a736c851437260fccb49041b448f24c7e5d7fcfc
Ulyana-AA <andreeva.ulyanakpc572020@gmail.com>
2023-02-01 20:08:09 +0300

    Добавьте файлы проекта.

---
+++ lab1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Program 
+{ 
+    class lab1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("а)  a c \n   -----;\n     b \n");
+            Console.WriteLine("б)  b \n   ---a;\n    c \n");
+            Console.WriteLine("в)  a \n   ---c;\n    b \n");
+            Console.WriteLine("г)      b \n   a + ---;\n        c \n");
+            Console.WriteLine("д) a + b \n   -----;\n     c \n");
+            Console.WriteLine("е)      b \n   a + --- + c;\n        b \n");
+            Console.WriteLine("ж) a + b \n   -----;\n   b + c \n");
+            Console.WriteLine("з)   a \n   -----;\n   sin b \n");
+            Console.WriteLine("и)  1 \n   ---ab sin x;\n    2 \n");
+            Console.WriteLine("к)           a\n            ---\n             c\n   2bc cos -----;\n           b + c \n");
+            Console.WriteLine("л)         a       b       c\n   4R sin --- sin --- sin ---;\n           2       2       2 \n");
+            Console.WriteLine("м) ax + b \n   ------;\n   cx + d \n");
+            Console.WriteLine("н)      a + b     a - b\n   2sin ----- cos -----;\n          2         2\n");
+            Console.WriteLine("о)            x \n   |2sin(-3)|---||.\n              2 \n");
+
+        }
+    }
+}
